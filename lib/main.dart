a99c8324f2d7917de0f78af1a375157381f69f54
panodpun <punn141@gmail.com>
2019-03-31 20:19:46 +0700

    #3 commit: add Product_Controler

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import './product_manager.dart';
+import 'product_manager.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange ,
+          accentColor:  Colors.deepPurple
+        ),
       home: Scaffold(
           appBar: AppBar(
             title: Text('EasyList'),
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
-
-  ProductManager(this.startingProduct){
+  ProductManager(this.startingProduct) {
     print('[ProductsManager Widget] Constructor');
   }
 
@@ -27,23 +26,30 @@ class _ProductManagerState extends State<ProductManager> {
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     print('[ProductsManager Widget] build()');
-    return Column(
-      children: [
-        Container(
-          margin: EdgeInsets.all(10.0),
-          child: RaisedButton(
-            onPressed: () {
-              setState(() {
-                _products.add('Advance Food Tester');
-              });
-            },
-            child: Text('Add Product'),
+    return ListView(children: <Widget>[
+      Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: ProductControl(_addProduct),
           ),
-        ),
-       Products(_products) 
-      ],
-    );
+          Products(_products)
+        ],
+      )
+    ]);
   }
 }
